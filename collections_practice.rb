d59f56305b38bcc29d7b5ed98bf0d0a855311802
Toni  Diep <adiep123@gmail.com>
2020-04-04 19:55:15 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,11 +20,11 @@ def reverse_array(array)
  end
 
 def kesha_maker(array)
-array.each do |word|
-  word[2]
+  array.each do |word|
+    word[2]
+  end
 end
-end
-end
+
 def find_a(a)
   #a.start_with
 end
